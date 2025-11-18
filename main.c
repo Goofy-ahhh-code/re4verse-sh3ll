6df5e39b49de661f24bed0e9d1d4d46fe7018b06
Gardner Sullivan <gardnersullivan9@gmail.com>
2025-11-18 12:17:30 -0700

    Add initial project configuration and server binary
    - Created project module configuration in .idea/modules.xml
    - Set up version control system mapping in .idea/vcs.xml
    - Added server binary file to the project

--- main.c
+++ main.c
@@ -4,10 +4,44 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
+int main() {
+    int server_fd;
+    int client_fd;
+    struct sockaddr_in addr;
+    char buffer[1024];
+    char *ip = "127.0.0.1";
+    int port = 8080;
+    int chat = 1;
 
-    int port = atoi(argv[1]);
-    printf("Server will run on port %d\n", port);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) { perror("socket"); exit(1); }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &addr.sin_addr);
 
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind"); exit(1);
+    }
+
+    if (listen(server_fd, 1) < 0) {
+        perror("listen"); exit(1);
+    }
+
+    while (chat == 1) {
+        client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd < 0) { perror("accept"); exit(1); }
+        while(1) {
+            int n = read(client_fd, buffer, sizeof(buffer)-1);
+            if (n > 0) {
+                buffer[n] = '\0';
+                printf("%s\n", buffer);
+                if (strcmp(buffer, "exit") == 0) {
+                    chat = 0;
+                }
+            }
+        }
+    }
+    close(client_fd);
+    close(server_fd);
     return 0;
 }
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 8080))  # store as variables later, maybe use command line arguments for that
 
-s.send("message".encode()) # figure out a username system as well as always sending with a newline character which is \n
-
+s.send("message\n".encode()) # figure out a username system
+s.send("exit".encode()) # figure out a username system
 s.close()
+
 
