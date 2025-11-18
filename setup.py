f5c1a73a15835319948cdfae117234699cb19a51
plainduck67 <gardnersullivan9@gmail.com>
2025-11-18 14:02:12 -0700

    Add subprocess calls to run server and main.py

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
+import subprocess
+import os
 
+os.system("gcc main.c -o server")
+subprocess.Popen(["gnome-terminal", "--", "./server", "8080"])
+subprocess.Popen(["gnome-terminal", "--", "python3", "main.py"]) 
