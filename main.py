c34c30a7771ab10444590b9341239e8b274250a1
Antimatter137 <148151420+antimatter137@users.noreply.github.com>
2025-11-18 12:37:10 -0700

    stuff
    Added functionality to take user input for username and messages, and send them over a socket connection.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import socket
+import os
 # i added these comments not chatgpt
+user = input("Enter your username: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 8080))  # store as variables later, maybe use command line arguments for that
+message = ""
+while message != "end":
+  message = input("Enter your message: ")
+  s.send(f"{user}: {message}".encode()) # figure out a username system
 
-s.send("message\n".encode()) # figure out a username system
-s.send("exit".encode()) # figure out a username system
+
+
 s.close()
-
 
