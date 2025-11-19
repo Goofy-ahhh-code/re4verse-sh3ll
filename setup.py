3c662cfba255934f1fbfd526ff75f50c1d6e0a48
Antimatter137 <148151420+antimatter137@users.noreply.github.com>
2025-11-18 17:01:15 -0700

    nuke it

--- setup.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import os
-
-os.system("gcc main.c -o server")
-subprocess.Popen(["gnome-terminal", "--", "./server", "8080"])
-subprocess.Popen(["gnome-terminal", "--", "python3", "main.py"]) 
